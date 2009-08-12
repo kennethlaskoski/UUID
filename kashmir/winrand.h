0574723feeecb9974f5f5dae90f75fb496c72ae1
kennethlaskoski <kennethlaskoski@fb5b2fd6-6a48-4efd-b20c-0e9efffbb194>
2009-08-12 03:03:40 +0000

    Apply patch contributed by Chet Stuut.

--- kashmir/winrand.h
+++ kashmir/winrand.h
@@ -1,11 +1,14 @@
 /********************************************************************\
- * winundocrand.h -- Windows random number generator                *
+ * winrand.h -- Windows random number generator                     *
  *                                                                  *
  * Copyright (C) 2008 Kenneth Laskoski                              *
  *                                                                  *
 \********************************************************************/
-/** @file winundocrand.h
+/** @file winrand.h
     @brief Windows random number generator
+    Uses the ADVAPI32!RtlGenRandom function, which doesn't require a CSP context
+    see <http://blogs.msdn.com/michael_howard/archive/2005/01/14/353379.aspx>
+    and <http://msdn.microsoft.com/en-us/library/aa387694%28VS.85%29.aspx>
     @author Copyright (C) 2008 Kenneth Laskoski
     based on work by
     @author Copyright (C) 1996, 1997, 1998 Theodore Ts'o
@@ -16,8 +19,8 @@
     LICENSE_1_0.txt or a copy at <http://www.boost.org/LICENSE_1_0.txt>.)
 */
 
-#ifndef KL_WINUNDOCRAND_H
-#define KL_WINUNDOCRAND_H
+#ifndef KL_WINRAND_H
+#define KL_WINRAND_H
 
 #include "randomstream.h"
 #include "noncopyable.h"
@@ -31,36 +34,36 @@
 namespace kashmir {
 namespace system {
 
-class WinUndocRand : public user::randomstream<WinUndocRand>, noncopyable
+class WinRand : public user::randomstream<WinRand>, noncopyable
 {
 public:
-    WinUndocRand() : hLib(LoadLibrary("ADVAPI32.DLL")), pfn(0)
+    WinRand() : hLib(LoadLibrary("ADVAPI32.DLL"))
     {
         if (!hLib)
             throw std::runtime_error("failed to load ADVAPI32.DLL.");
 
-        pfn = (BOOLEAN (APIENTRY *)(void*,ULONG)) GetProcAddress(hLib,"SystemFunction036");
-        if (!pfn)
+        RtlGenRandom = (BOOLEAN (APIENTRY *)(void*,ULONG)) GetProcAddress(hLib,"SystemFunction036");
+        if (!RtlGenRandom)
         {
             FreeLibrary(hLib);
             throw std::runtime_error("failed to get ADVAPI32!RtlGenRandom address.");
         }
     }
 
-    ~WinUndocRand()
+    ~WinRand()
     {
         FreeLibrary(hLib);
     }
 
     void read(char* buffer, std::size_t count)
     {
-        if (!pfn(buffer, count))
+        if (!RtlGenRandom(buffer, count))
             throw std::runtime_error("system failed to generate random data.");
     }
 
 private:
     HMODULE hLib;
-    BOOLEAN (APIENTRY *pfn)(void*, ULONG);
+    BOOLEAN (APIENTRY *RtlGenRandom)(void*, ULONG);
 };
 
 }}
