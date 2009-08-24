998bcfcab0454444759ad1bb1a14495b4c0a94c1
kennethlaskoski <kennethlaskoski@fb5b2fd6-6a48-4efd-b20c-0e9efffbb194>
2009-08-24 02:28:04 +0000

    Keeping symmmetry.

--- kashmir/winrandom.h
+++ kashmir/winrandom.h
@@ -1,21 +1,17 @@
-/********************************************************************\
- * winrandom.h -- Windows random number generator                   *
- *                                                                  *
- * Copyright (C) 2008 Kenneth Laskoski                              *
- *                                                                  *
-\********************************************************************/
+// winrandom.h -- Windows random number generator
+
+// Copyright (C) 2008 Kenneth Laskoski
+
 /** @file winrandom.h
     @brief Windows random number generator
     @author Copyright (C) 2008 Kenneth Laskoski
-    based on work by
-    @author Copyright (C) 1996, 1997, 1998 Theodore Ts'o
-    @author Copyright (C) 2004-2008 Ralf S. Engelschall <rse@engelschall.com>
+
     with contribution of
     @author Chet Stuut
 
-    Use, modification, and distribution are subject
-    to the Boost Software License, Version 1.0.  (See accompanying file
-    LICENSE_1_0.txt or a copy at <http://www.boost.org/LICENSE_1_0.txt>.)
+    Use, modification, and distribution are subject to the
+    Boost Software License, Version 1.0. See accompanying file
+    LICENSE_1_0.txt or <http://www.boost.org/LICENSE_1_0.txt>.
 */
 
 #ifndef KL_WINRANDOM_H 
@@ -39,7 +35,7 @@ class WinRandom : public user::randomstream<WinRandom>, unique<WinRandom>
     HCRYPTPROV hProv;
 
 public:
-    WinRandom(HCRYPTPROV hProv = NULL) : hProv(hProv)
+    WinRandom()
     {
         if (!CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_FULL, 0))
             if (GetLastError() != NTE_BAD_KEYSET || !CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_FULL, CRYPT_NEWKEYSET))
