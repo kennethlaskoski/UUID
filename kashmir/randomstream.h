87aca77bf33bf7ca7c93d005a533f24edb143c22
Kenneth Laskoski <kennethlaskoski@users.noreply.github.com>
2013-01-13 02:48:20 +0000

    crtp stands for curiously recurring template parameter

--- kashmir/randomstream.h
+++ kashmir/randomstream.h
@@ -14,37 +14,37 @@
 
 namespace kashmir {
 
-template<class user_impl>
+template<class crtp_impl>
 class randomstream
 {
-    user_impl* const self;
+    crtp_impl* const self;
 
 public:
-    randomstream<user_impl>() : self(static_cast<user_impl*>(this)) {}
+    randomstream<crtp_impl>() : self(static_cast<crtp_impl*>(this)) {}
 
     void read(char* buffer, std::size_t count)
     {
         self->read(buffer, count);
     }
 
-    randomstream<user_impl>& operator>>(char& c) { read(&c, 1); return *this; }
-    randomstream<user_impl>& operator>>(signed char& c) { read(reinterpret_cast<char*>(&c), 1); return *this; }
-    randomstream<user_impl>& operator>>(unsigned char& c) { read(reinterpret_cast<char*>(&c), 1); return *this; }
+    randomstream<crtp_impl>& operator>>(char& c) { read(&c, 1); return *this; }
+    randomstream<crtp_impl>& operator>>(signed char& c) { read(reinterpret_cast<char*>(&c), 1); return *this; }
+    randomstream<crtp_impl>& operator>>(unsigned char& c) { read(reinterpret_cast<char*>(&c), 1); return *this; }
 
-    randomstream<user_impl>& operator>>(int& n) { read(reinterpret_cast<char*>(&n), sizeof(int)); return *this; }
-    randomstream<user_impl>& operator>>(long& n) { read(reinterpret_cast<char*>(&n), sizeof(long)); return *this; }
-    randomstream<user_impl>& operator>>(short& n) { read(reinterpret_cast<char*>(&n), sizeof(short)); return *this; }
+    randomstream<crtp_impl>& operator>>(int& n) { read(reinterpret_cast<char*>(&n), sizeof(int)); return *this; }
+    randomstream<crtp_impl>& operator>>(long& n) { read(reinterpret_cast<char*>(&n), sizeof(long)); return *this; }
+    randomstream<crtp_impl>& operator>>(short& n) { read(reinterpret_cast<char*>(&n), sizeof(short)); return *this; }
 
-    randomstream<user_impl>& operator>>(unsigned int& u) { read(reinterpret_cast<char*>(&u), sizeof(unsigned int)); return *this; }
-    randomstream<user_impl>& operator>>(unsigned long& u) { read(reinterpret_cast<char*>(&u), sizeof(unsigned long)); return *this; }
-    randomstream<user_impl>& operator>>(unsigned short& u) { read(reinterpret_cast<char*>(&u), sizeof(unsigned short)); return *this; }
+    randomstream<crtp_impl>& operator>>(unsigned int& u) { read(reinterpret_cast<char*>(&u), sizeof(unsigned int)); return *this; }
+    randomstream<crtp_impl>& operator>>(unsigned long& u) { read(reinterpret_cast<char*>(&u), sizeof(unsigned long)); return *this; }
+    randomstream<crtp_impl>& operator>>(unsigned short& u) { read(reinterpret_cast<char*>(&u), sizeof(unsigned short)); return *this; }
 
-    randomstream<user_impl>& operator>>(float& f) { read(reinterpret_cast<char*>(&f), sizeof(float)); return *this; }
-    randomstream<user_impl>& operator>>(double& f) { read(reinterpret_cast<char*>(&f), sizeof(double)); return *this; }
-    randomstream<user_impl>& operator>>(long double& f) { read(reinterpret_cast<char*>(&f), sizeof(long double)); return *this; }
+    randomstream<crtp_impl>& operator>>(float& f) { read(reinterpret_cast<char*>(&f), sizeof(float)); return *this; }
+    randomstream<crtp_impl>& operator>>(double& f) { read(reinterpret_cast<char*>(&f), sizeof(double)); return *this; }
+    randomstream<crtp_impl>& operator>>(long double& f) { read(reinterpret_cast<char*>(&f), sizeof(long double)); return *this; }
 
-    randomstream<user_impl>& operator>>(bool& b) { read(reinterpret_cast<char*>(&b), sizeof(bool)); b &= 1; return *this; }
-    randomstream<user_impl>& operator>>(void*& p) { read(reinterpret_cast<char*>(&p), sizeof(void*)); return *this; }
+    randomstream<crtp_impl>& operator>>(bool& b) { read(reinterpret_cast<char*>(&b), sizeof(bool)); b &= 1; return *this; }
+    randomstream<crtp_impl>& operator>>(void*& p) { read(reinterpret_cast<char*>(&p), sizeof(void*)); return *this; }
 };
 
 } // namespace kashmir
