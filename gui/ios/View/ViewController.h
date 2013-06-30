2a5bc290388e2d707849eb2c2a26d9ec9cde1774
kennethlaskoski <kennethlaskoski@fb5b2fd6-6a48-4efd-b20c-0e9efffbb194>
2013-06-30 08:49:10 +0000

    Move ViewController code to implementation file

--- gui/ios/View/ViewController.h
+++ gui/ios/View/ViewController.h
@@ -10,15 +10,4 @@
 
 @interface ViewController : UIViewController <UITextFieldDelegate>
 
-@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
-@property (weak, nonatomic) IBOutlet UITextField *nTextField;
-@property (weak, nonatomic) IBOutlet UITextView *textView;
-
-- (IBAction)generateNIL:(id)sender;
-- (IBAction)generateNS:(id)sender;
-- (IBAction)generateV3:(id)sender;
-- (IBAction)generateV4:(id)sender;
-- (IBAction)generateV5:(id)sender;
-- (IBAction)clearContent:(id)sender;
-
 @end
